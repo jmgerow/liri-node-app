7cfe378ddfc4225e92189c184ffde602913452f4
jmgerow <justingerow1@gmail.com>
2018-07-29 14:42:46 -0500

    added spotify and do what it says

--- liri.js
+++ liri.js
@@ -1,44 +1,65 @@
 require("dotenv").config();
-
+//TODO: need to fix "keys" showing as undefined
 // var spotify = new Spotify(keys.spotify);
 // var client = new Twitter(keys.twitter);
 
+//TODO: need song search and movie search to read default values if field is blank
+
 //setting up spotify search
-
 if (process.argv[2] === "spotify-this-song") {
+  spotify()
+};
+
+function spotify() {
   var Spotify = require('node-spotify-api');
+  var songName = "";
+  console.log('songName', songName)
+  var nodeArgs = process.argv;
+  for (var i = 3; i < nodeArgs.length; i++) {
 
+    songName = songName + " " + nodeArgs[i];
+    console.log('songName', songName)
+  }
+  // var spotify = new Spotify(keys.spotify);
   var spotify = new Spotify({
-    id: '76d99e26a2b24f96b161ac14ff899152',
-    secret: '1408764b1455475ba9b552c90bf703cf'
+    id: (process.env.SPOTIFY_ID),
+    secret: (process.env.SPOTIFY_SECRET)
   });
-
-  spotify.search({ type: 'track', query: process.argv[3] }, function (err, data) {
+  console.log('songName', songName)
+  spotify.search({ type: 'track', query: songName, limit: 1 }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
 
-    console.log(data);
-  });
 
+    // console.log(data);   
+    // console.log(JSON.stringify(data, null, 2));
+    console.log("Arist: " + (JSON.stringify((data.tracks.items[0].album.artists[0].name), null, 2)));
+    console.log("Song Title: " + (JSON.stringify((data.tracks.items[0].name), null, 2)));
+    console.log("Preview Link: " + (JSON.stringify((data.tracks.items[0].album.artists[0].external_urls.spotify), null, 2)));
+    console.log("Album: " + (JSON.stringify((data.tracks.items[0].album.name), null, 2)));
+  });
 };
 
 //setting up OMDB search
-
 if (process.argv[2] === "movie-this") {
+  movie()
+};
+
+function movie() {
   var request = require("request");
   var movieName = "";
   var nodeArgs = process.argv;
   for (var i = 3; i < nodeArgs.length; i++) {
 
     movieName = movieName + " " + nodeArgs[i];
-  
+
   }
 
   var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
   console.log(queryUrl);
 
-  request(queryUrl, function(error, response, body) {
+  request(queryUrl, function (error, response, body) {
 
     if (!error && response.statusCode === 200) {
       console.log("Title: " + JSON.parse(body).Title);
@@ -51,4 +72,95 @@ if (process.argv[2] === "movie-this") {
       console.log("Actors: " + JSON.parse(body).Actors);
     }
   });
-}; 
+};
+
+//setting up "do what it says"
+
+//*****IMPORTANT! Must use "node liri.js random" to pull correctly******
+var fs = require("fs");
+
+fs.readFile("random.txt", "utf8", function (error, data) {
+
+  if (error) {
+    return console.log(error);
+  }
+
+  // console.log(data);
+
+  var dataArr = data.split(",");
+
+  // console.log(dataArr);
+
+  if (dataArr[0] === "spotify-this-song" && process.argv[2] === "random") {
+    var Spotify = require('node-spotify-api');
+    var songName = dataArr[1];
+
+    // var spotify = new Spotify(keys.spotify);
+    var spotify = new Spotify({
+      id: (process.env.SPOTIFY_ID),
+      secret: (process.env.SPOTIFY_SECRET)
+    });
+
+    spotify.search({ type: 'track', query: songName, limit: 1 }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      // console.log(data);   
+      // console.log(JSON.stringify(data, null, 2));
+      console.log("Arist: " + (JSON.stringify((data.tracks.items[0].album.artists[0].name), null, 2)));
+      console.log("Song Title: " + (JSON.stringify((data.tracks.items[0].name), null, 2)));
+      console.log("Preview Link: " + (JSON.stringify((data.tracks.items[0].album.artists[0].external_urls.spotify), null, 2)));
+      console.log("Album: " + (JSON.stringify((data.tracks.items[0].album.name), null, 2)));
+    });
+
+  }
+  if (dataArr[0] === "movie-this" && process.argv[2] === "random") {
+    var request = require("request");
+    var movieName = dataArr[1];
+  
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
+
+    request(queryUrl, function (error, response, body) {
+
+      if (!error && response.statusCode === 200) {
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+      }
+    });
+
+  }
+
+
+});
+
+//setting up Twitter API
+if (process.argv[2] === "my-tweets") {
+  myTweets()
+};
+
+function myTweets() {
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {jmgerow: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) { 
+  if (!error) {
+    console.log('tweets', tweets);
+    console.log('response', response)
+  }
+});
+};
